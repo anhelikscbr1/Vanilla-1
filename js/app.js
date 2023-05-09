4a12d26969229e534b78bca05840236264d41704
anhelikscbr1 <123397822+anhelikscbr1@users.noreply.github.com>
2023-05-09 10:44:32 -0600

    se agregan funciones a js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,91 @@
 console.log('conectado')
+
+let carrito = {}
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+
+document.addEventListener('DOMContentLoaded', e => {
+    cargaDatosBD()
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        pintarCarrito()
+    }
+})
+
+cards.addEventListener('click', e => {
+    addCarrito(e)
+ })
+
+ const addCarrito = e => {
+    if(e.target.classList.contains('btn-dark')){
+        setCarrito(e.target.parentElement)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    }
+    e.stopPropagation()
+}
+
+const addCarrito = e => {
+    if(e.target.classList.contains('btn-dark')){
+        setCarrito(e.target.parentElement)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    }
+    e.stopPropagation()
+}
+
+
+const setCarrito = item => {
+    const producto = {
+        id: item.querySelector('button').dataset.id,
+        title: item.querySelector('h5').textContent,
+        precio: item.querySelector('p').textContent,
+        cantidad: 1
+    }
+
+    if(carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = { ...producto }
+    pintarCarrito()
+}
+
+const pintarCarrito = () => {
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.title
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+
+        templateCarrito.querySelector('.btn-success').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+    pintarFooter()
+}
+
+const cargaDatosBD = async () => {
+    const res = await fetch('../db/api.json')
+    const data = await res.json()
+    pintarCards(data)
+}
+
+const pintarCards = (data) => {
+    data.forEach(item => {
+        console.log(item)
+        templateCard.querySelector('h5').textContent = item.title
+        templateCard.querySelector('p').textContent = item.precio
+        templateCard.querySelector('button').dataset.id = item.id
+        templateCard.querySelector('img').setAttribute("src", item.imageUrl)
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cards.appendChild(fragment)
+}
